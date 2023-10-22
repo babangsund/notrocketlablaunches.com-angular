a88f89b8d8624666982231db62891ea11e27cc1d
Benjamin Bangsund <babangsund@gmail.com>
2023-10-22 14:25:59 -0700

    Add eslint, stylelint, prettier and husky

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    standalone: true,
+    imports: [CommonModule, RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'notrocketlablaunches.com-angular';
+    title = 'notrocketlablaunches.com-angular';
 }
